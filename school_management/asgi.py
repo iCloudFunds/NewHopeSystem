107084f791cdfeb3362d43b853a297b3093f418a
iCloudFunds <nde.kenneth61@gmail.com>
2025-12-31 23:55:42 +0100

    Finalize Render deployment config with Daphne and school_management paths

---
+++ school_management/asgi.py
@@ -0,0 +1,16 @@
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_management.settings')
+
+application = get_asgi_application()import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_management.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # We will add "websocket" routing here once the chat logic is ready
+})
