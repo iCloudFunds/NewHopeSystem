26273b5e74f3de4f0e1f6616bc5835856f5eb4a7
iCloudFunds <nde.kenneth61@gmail.com>
2026-01-01 00:43:16 +0100

    Update department models to use settings.AUTH_USER_MODEL

--- departments/models.py
+++ departments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
 class Department(models.Model):
     name = models.CharField(max_length=100, unique=True)
     code = models.CharField(max_length=10, unique=True)
